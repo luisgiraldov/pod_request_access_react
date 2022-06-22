b7daa0b2cabf3d08f956b6484ad70bfb0f3bde49
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2022-06-22 16:21:05 -0400

    Added sanitize function

--- src/helper_functions.js
+++ src/helper_functions.js
@@ -1,12 +1,26 @@
 function emailValidator() {
     const email = document.getElementById("emailField");
     const emailRegex = /^[^@]+@[^@.]+\.[a-z]+$/i;
-    let emailValue = email.value;
+    let emailValue = sanitize(email.value);
     if(emailRegex.test(emailValue)) {
         return true;
     }
 
     return false;
 }
+
+function sanitize(string) {
+    const map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#x27;',
+        "/": '&#x2F;',
+        "`": '&grave;',
+    };
+    const reg = /[&<>"'/`]/ig;
+    return string.replace(reg, (match)=>(map[match]));
+  }
 
 export { emailValidator };
